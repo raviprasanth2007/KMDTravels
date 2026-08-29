0dd53fbb5a8d6e268785d0e0a9db81ddf3f3c083
Ravi Prasanth <raviprasanth2007@gmail.com>
2026-08-29 22:40:04 +0530

    Newly updated code without error

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/KMDTravels/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
